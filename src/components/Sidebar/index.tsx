31e70690e8d0e2ebbedbc5e08ba2091c823ff6ce
Shady <clownclowed@gmail.com>
2023-06-29 20:13:13 +0800

    Update size and icon in Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,7 @@
 import { type ReactElement } from 'react';
 
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import BedtimeIcon from '@mui/icons-material/Bedtime';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { Form } from 'react-router-dom';
 
@@ -13,7 +13,7 @@ export default function Sidebar(): ReactElement {
   const user = useUser();
 
   return (
-    <div className='flex h-full w-48 flex-col gap-9 bg-[#2E2E2E] px-1 py-8'>
+    <div className='flex h-full w-56 flex-col gap-9 bg-[#2E2E2E] px-3 py-8'>
       <div className='-mx-2 mt-6 flex flex-col items-center'>
         <img
           src={user.avatarfull}
@@ -24,7 +24,7 @@ export default function Sidebar(): ReactElement {
       <div className='flex flex-1 flex-col justify-between'>
         <div className='font-bold text-white'>
           <CustomNavLink to='/'>
-            <BedtimeIcon />
+            <LibraryBooksIcon />
             <span>My Library</span>
           </CustomNavLink>
         </div>
