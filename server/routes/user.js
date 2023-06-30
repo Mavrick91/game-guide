1107fc5b8cab609ef9d7fcc767b103319c6ad27d
Shady <clownclowed@gmail.com>
2023-06-30 12:17:08 +0800

    Implement fetching list of friends

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,30 +3,36 @@ const verifyJwtMiddleware = require('../middleware/verifyJwt');
 const axios = require('axios');
 const { convertMinutesToTime } = require('../utils/format');
 
-const STEAM_API_URL = 'http://api.steampowered.com/IPlayerService';
+const STEAM_API_URL = 'http://api.steampowered.com';
 const STEAM_MEDIA_URL =
   'http://media.steampowered.com/steamcommunity/public/images/apps';
 const GEONAMES_API_URL = 'http://api.geonames.org/searchJSON';
 
 const router = express.Router();
 
-router.get('/isAuthenticated', (req, res) => {
+router.get('/isAuthenticated', isAuthenticated);
+router.use(verifyJwtMiddleware);
+router.get('/me', getProfile);
+router.post('/logout', logout);
+router.get('/location', getLocation);
+router.get('/games', getGames);
+router.get('/friends', getFriends);
+
+function isAuthenticated(req, res) {
   const token = req.cookies.jwt;
   return res.status(200).json(!!token);
-});
+}
 
-router.use(verifyJwtMiddleware);
-
-router.get('/me', (req, res) => {
+function getProfile(req, res) {
   res.status(200).json(req.user);
-});
+}
 
-router.post('/logout', (req, res) => {
+function logout(req, res) {
   res.clearCookie('jwt');
   res.sendStatus(200);
-});
+}
 
-router.get('/location', async (req, res, next) => {
+async function getLocation(req, res, next) {
   try {
     const { loccountrycode, locstatecode, loccityid } = req.user;
 
@@ -59,9 +65,9 @@ router.get('/location', async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-});
+}
 
-router.get('/games', async (req, res, next) => {
+async function getGames(req, res, next) {
   try {
     const { steamid } = req.user;
     const params = {
@@ -73,8 +79,13 @@ router.get('/games', async (req, res, next) => {
     };
 
     const [gamesOwned, recentlyPlayed] = await Promise.all([
-      axios.get(`${STEAM_API_URL}/GetOwnedGames/v0001/`, { params }),
-      axios.get(`${STEAM_API_URL}/GetRecentlyPlayedGames/v0001/`, { params }),
+      axios.get(`${STEAM_API_URL}/IPlayerService/GetOwnedGames/v0001/`, {
+        params,
+      }),
+      axios.get(
+        `${STEAM_API_URL}/IPlayerService/GetRecentlyPlayedGames/v0001/`,
+        { params }
+      ),
     ]);
 
     const ownedGames =
@@ -86,7 +97,47 @@ router.get('/games', async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-});
+}
+
+async function getFriends(req, res, next) {
+  try {
+    const { steamid } = req.user;
+    const params = {
+      steamid,
+      key: process.env.STEAM_API_KEY,
+      relationship: 'all',
+      format: 'json',
+    };
+
+    const { data } = await axios.get(
+      `${STEAM_API_URL}/ISteamUser/GetFriendList/v0001/`,
+      { params }
+    );
+
+    const friendsPromise = data.friendslist.friends.map((friend) => {
+      return axios.get(
+        `${STEAM_API_URL}/ISteamUser/GetPlayerSummaries/v0002/`,
+        {
+          params: {
+            steamids: friend.steamid,
+            key: process.env.STEAM_API_KEY,
+            format: 'json',
+          },
+        }
+      );
+    });
+
+    const friendsResponse = await Promise.all(friendsPromise);
+    const friends = friendsResponse.map((friend, index) => ({
+      ...friend.data.response.players[0],
+      friend_since: data.friendslist.friends[index].friend_since,
+    }));
+
+    res.status(200).json({ friends });
+  } catch (error) {
+    next(error);
+  }
+}
 
 function formatGameData(game) {
   return {
