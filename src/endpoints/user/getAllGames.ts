0dadcb1f908116dcd771c445f278e7dd5a2ba9da
Shady <clownclowed@gmail.com>
2023-07-03 19:21:45 +0800

    Update query to fetch all games

---
+++ src/endpoints/user/getAllGames.ts
@@ -0,0 +1,23 @@
+import axios from '../../config/axios';
+import { type CustomQuery } from '../../types/useQuery';
+
+export interface AllGames {
+  appid: number;
+  name: string;
+}
+
+interface GamesAPI {
+  applist: {
+    apps: AllGames[];
+  };
+}
+export default function getAllGames(): CustomQuery<AllGames[]> {
+  return {
+    queryKey: ['allGames'],
+    queryFn: async () => {
+      const { data } = await axios.get<GamesAPI>('/user/all-games');
+
+      return data.applist.apps;
+    },
+  };
+}
