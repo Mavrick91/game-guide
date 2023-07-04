af1a1b4d1bd74037f5ab1c44cf9effb8dac8ba28
Shady <clownclowed@gmail.com>
2023-07-04 15:52:14 +0800

    Redirect to the game details page

--- src/components/ui/PopupAllGames.tsx
+++ src/components/ui/PopupAllGames.tsx
@@ -1,17 +1,23 @@
-import { type ReactElement, useDeferredValue } from "react";
+import { type ReactElement, useDeferredValue } from 'react';
 
-import useSearchedGames from "../../hooks/useSearchedGames";
+import { Link } from 'react-router-dom';
 
-export default function PopupAllGames({ input }: { input: string }): ReactElement {
+import useSearchedGames from '../../hooks/useSearchedGames';
+
+export default function PopupAllGames({
+  input,
+}: {
+  input: string;
+}): ReactElement {
   const deferredQuery = useDeferredValue(input);
   const searchedGames = useSearchedGames(deferredQuery);
 
   return (
     <div className='no-scrollbar absolute inset-x-0 top-full z-20 mt-2 max-h-80 overflow-y-scroll rounded-xl bg-[#2E2E2E] shadow-2xl drop-shadow-lg'>
       {searchedGames?.map((game) => (
-        <div key={game.appid} className='px-6 py-4'>
-          {game.name}
-        </div>
+        <Link to={`/game/${game.appid}`} key={game.appid}>
+          <div className='px-6 py-4'>{game.name}</div>
+        </Link>
       ))}
     </div>
   );
